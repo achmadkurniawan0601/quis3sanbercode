99e57610a437570f26683d671b5179efb869ac23
achmadkurniawan0601 <achmad.kurniawan0601@gmail.com>
2023-09-20 15:53:57 +0700

    Membuat Test Case Positif, Negatif, Test Collection, dan Test Suite untuk Login, dan Register

---
+++ Sanbercode-Katalon-AchmadKurniawan/Object Repository/rgstr/rgstr_username.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>rgstr_username</name>
+   <tag></tag>
+   <elementGuidId>4605fd17-2b88-4ddf-9343-bf18c5e7d399</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'sign-username']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>sign-username</value>
+      <webElementGuid>f72ce82d-3b71-4c8f-82c1-fd108bf407ec</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Sanbercode-Katalon-AchmadKurniawan/Test Suites/login/suite_login.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>suite_login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>890c69bc-df09-487a-a7a4-13d7801b3c49</testSuiteGuid>
+   <testCaseLink>
+      <guid>14aecc40-f055-4faa-8481-36495a36fe54</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login/InvalidLogin</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>27ca60d2-2729-4e4f-b9ee-d28c4b70de0e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login/LoginValid</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
