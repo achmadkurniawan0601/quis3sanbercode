e0ba27bdbe7a6be3c5687474f1ba13f93a7b33cf
achmadkurniawan0601 <achmad.kurniawan0601@gmail.com>
2023-09-21 13:14:33 +0700

    Login valid, dan invalid menggunakan data dar Excel

--- Sanbercode-Katalon-AchmadKurniawan/Test Suites/login/suite_login.ts
+++ Sanbercode-Katalon-AchmadKurniawan/Test Suites/login/suite_login.ts
@@ -12,17 +12,24 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>890c69bc-df09-487a-a7a4-13d7801b3c49</testSuiteGuid>
    <testCaseLink>
-      <guid>14aecc40-f055-4faa-8481-36495a36fe54</guid>
+      <guid>786f7099-c48b-4e48-9d25-06a19104767f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/login/InvalidLogin</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>27ca60d2-2729-4e4f-b9ee-d28c4b70de0e</guid>
+      <guid>3b6e0a3f-4f6b-4b21-af09-4f03d91395c2</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/login/LoginValid</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e58db95a-88f9-4888-8b6f-24534155955a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login/loopLoginExcel</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
